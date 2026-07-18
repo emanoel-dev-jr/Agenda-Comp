032f3fa74826d0dd8b5b76823616903eceb04442
Emanoel Brian Maffei <emanoel.maffei@hotmail.com>
2026-07-18 13:54:33 -0300

    Adiciona xlsx com pagina web

---
+++ Aulas/Aula-06_18-07-2026/xlsx-na-web/script.js
@@ -0,0 +1,42 @@
+// import XLSX from "xlsx";
+
+const inputArquivo = document.getElementById("input-arquivo");
+
+inputArquivo.addEventListener("change", renderizarTabela);
+
+function renderizarTabela(event) {
+    const arquivo = event.target.files[0];
+    if (!arquivo) return;
+
+    const leitor = new FileReader();
+
+    // Método disparado quando o arquivo acabar de ser lido
+    leitor.onload = (e) => {
+        // Convertendo os dados brutos lidos pelo leitor (zeros e uns) em um formato compreendido pelo JavaScript
+
+        //
+        // U -> Unsigned
+        // int -> Integer
+        // 8 -> 8 bits
+        // Array -> Array padrão
+        //
+        const dadosBrutos = new Uint8Array(e.target.result);
+
+        // Lendo o arquivo (os dados brutos adquiridos)
+        // |
+        // |------------------------------------- configuração -
+        // |-------------------- código binário-----------------
+        const arquivo = XLSX.read(dadosBrutos, { type: "array" });
+    
+        const nomePlanilha1 = arquivo.SheetNames[0];
+        const planilha1 = arquivo.Sheets[nomePlanilha1];
+
+        // Convertendo a planilha para o formato HTML (gera tabela automaticamente)
+        const tabelaHTML = XLSX.utils.sheet_to_html(planilha1);
+        console.log(planilha1)
+
+        document.body.innerHTML += tabelaHTML;
+    }
+    
+    leitor.readAsArrayBuffer(arquivo);
+}
