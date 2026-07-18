8339ce36c1bb241c05d394dfbdc90508c3a696de
Emanoel Brian Maffei <emanoel.maffei@hotmail.com>
2026-07-18 10:58:41 -0300

    Adiciona conteudo a aula 06

--- Aulas/Aula-06_18-07-2026/sheet_to_json.js
+++ Aulas/Aula-06_18-07-2026/sheet_to_json.js
@@ -1,7 +1,23 @@
 import XLSX from "xlsx";
 
-// Armazenando um "livro" (arquivo) já existente em uma variável
+// Armazenando um "livro" (arquivo) já existente
 const workbook = XLSX.readFile("./lista_usuarios.xlsx");
 
-// Acessando e imprimindo a lista de nomes das páginas
-console.log(workbook.SheetNames)
+// Armazenando a página Usuários
+const abaUsuarios = workbook.Sheets["Usuários"];
+
+// Convertendo a página armazenada para um objeto JSON
+const abaUsuariosJSON = XLSX.utils.sheet_to_json(abaUsuarios);
+
+// Imprimindo a lista de nomes das páginas
+console.log("\nPáginas:", workbook.SheetNames);
+
+console.log("==================================================")
+
+// Imprimindo a abas/página Usuários
+console.log("\nPágina Usuário:\n\n", abaUsuarios);
+
+console.log("==================================================")
+
+// Imprimindo a abas/página Usuários em formato JSON
+console.log("\nJSON criado da : \n\n", abaUsuariosJSON);
